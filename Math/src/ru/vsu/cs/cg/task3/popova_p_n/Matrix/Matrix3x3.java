050a5d9bfd06c6264b37adac2e63f57c4494b616
Polina Popova <popovapolina2133@gmail.com>
2023-11-02 02:20:01 +0300

    add

--- Math/src/ru/vsu/cs/cg/task3/popova_p_n/Matrix/Matrix3x3.java
+++ Math/src/ru/vsu/cs/cg/task3/popova_p_n/Matrix/Matrix3x3.java
@@ -1,2 +1,54 @@
-package ru.vsu.cs.cg.task3.popova_p_n.Matrix;public class Matrix3x3 {
+package ru.vsu.cs.cg.task3.popova_p_n.Matrix;
+
+public class Matrix3x3 {
+
+    private int[][] matrix;
+    public int[][] getMatrix() {
+        return matrix;
+    }
+    public Matrix3x3(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public static Matrix3x3 addition(Matrix3x3[] matrix) {
+        int[][] res = new int[3][3];
+        for (Matrix3x3 m : matrix) {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    res[i][j] += m.matrix[i][j];
+                }
+            }
+        }
+        return new Matrix3x3(res);
+    }
+
+    public static Matrix3x3 subtraction(Matrix3x3[] matrix) {
+        int[][] res = new int[3][3];
+        for (Matrix3x3 m : matrix) {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    res[i][j] -= m.matrix[i][j];
+                }
+            }
+        }
+        return new Matrix3x3(res);
+    }
+    public static Matrix3x3 multiplication(Matrix3x3[] matrices) {
+        int[][] result = new int[3][3];
+        for (Matrix3x3 m : matrices) {
+            int[][] currentMatrix = m.getMatrix();
+            int[][] newResult = new int[3][3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    for (int k = 0; k < 3; k++) {
+                        newResult[i][j] += result[i][k] * currentMatrix[k][j];
+                    }
+                }
+            }
+            result = newResult;
+        }
+        return new Matrix3x3(result);
+    }
+
 }
+
--- Math/tests/ru/vsu/cs/cg/math/MathTest.java
+++ Math/tests/ru/vsu/cs/cg/math/MathTest.java
@@ -1,2 +1,46 @@
-package ru.vsu.cs.cg.math;public class MathTest {
+package ru.vsu.cs.cg.math;
+
+import org.junit.jupiter.api.Test;
+import ru.vsu.cs.cg.task3.popova_p_n.Matrix.Matrix3x3;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MathTest {
+    @Test
+    void testAddition() {
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix2 = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+        Matrix3x3 m1 = new Matrix3x3(matrix1);
+        Matrix3x3 m2 = new Matrix3x3(matrix2);
+        Matrix3x3 result = Matrix3x3.addition(new Matrix3x3[] {m1, m2});
+
+        int[][] expectedResult = {{10, 10, 10}, {10, 10, 10}, {10, 10, 10}};
+        assertArrayEquals(expectedResult, result.getMatrix());
+    }
+
+    @Test
+    void testSubtraction() {
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix2 = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+        Matrix3x3 m1 = new Matrix3x3(matrix1);
+        Matrix3x3 m2 = new Matrix3x3(matrix2);
+
+        Matrix3x3 result = Matrix3x3.subtraction(new Matrix3x3[] {m1, m2});
+
+        int[][] expectedResult = {{-8, -6, -4}, {-2, 0, 2}, {4, 6, 8}};
+        assertArrayEquals(expectedResult, result.getMatrix());
+    }
+
+    @Test
+    void testMultiplication() {
+        int[][] matrix1 = {{3,2,1}, {2,5,3}, {3,4,2}};
+        int[][] matrix2 = {{3,2,1}, {2,5,3}, {3,4,2}};
+        Matrix3x3 m1 = new Matrix3x3(matrix1);
+        Matrix3x3 m2 = new Matrix3x3(matrix2);
+
+        Matrix3x3 result = Matrix3x3.multiplication(new Matrix3x3[] {m1, m2});
+
+        int[][] expectedResult = {{16,20,11}, {25,41,23}, {23,34,19}};
+        assertArrayEquals(expectedResult, result.getMatrix());
+    }
 }
