2a456492bf68e684b558fc6ae7b01a264cdcf41e
Polina Popova <popovapolina2133@gmail.com>
2023-11-02 21:32:30 +0300

    add new methods

--- Math/src/ru/vsu/cs/cg/task3/popova_p_n/Matrix/Matrix3x3.java
+++ Math/src/ru/vsu/cs/cg/task3/popova_p_n/Matrix/Matrix3x3.java
@@ -1,5 +1,7 @@
 package ru.vsu.cs.cg.task3.popova_p_n.Matrix;
 
+import org.junit.platform.commons.util.ToStringBuilder;
+
 public class Matrix3x3 {
 
     private int[][] matrix;
@@ -33,22 +35,49 @@ public class Matrix3x3 {
         }
         return new Matrix3x3(res);
     }
-    public static Matrix3x3 multiplication(Matrix3x3[] matrices) {
-        int[][] result = new int[3][3];
-        for (Matrix3x3 m : matrices) {
-            int[][] currentMatrix = m.getMatrix();
-            int[][] newResult = new int[3][3];
+    public static Matrix3x3 multiplication(Matrix3x3[] matrix) {
+        int[][] res = matrix[0].getMatrix();
+        for (int m = 1; m < matrix.length; m++) {
+            int[][] matriX = matrix[m].getMatrix();
+            int[][] newRes = new int[3][3];
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     for (int k = 0; k < 3; k++) {
-                        newResult[i][j] += result[i][k] * currentMatrix[k][j];
+                        newRes[i][j] += res[i][k] * matriX[k][j];
                     }
                 }
             }
-            result = newResult;
+            res = newRes;
         }
-        return new Matrix3x3(result);
+        return new Matrix3x3(res);
+    }
+    public static Matrix3x3 transposition(Matrix3x3 matrix){
+        int[][] res = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                res[i][j] = matrix.matrix[j][i];
+            }
+        }
+        return new Matrix3x3(res);
     }
 
+    public static Matrix3x3 zeroMatrix() {
+        int[][] zeroMatrix = new int[3][3];
+        return new Matrix3x3(zeroMatrix);
+    }
+
+    public static Matrix3x3 unitMatrix() {
+        int[][] res = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j){
+                    res[i][j] = 1;
+                } else {
+                    res[i][j] = 0;
+                }
+            }
+        }
+        return new Matrix3x3(res);
+    }
 }
 
